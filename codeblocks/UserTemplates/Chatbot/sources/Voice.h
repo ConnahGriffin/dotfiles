b0126a60bba75d240d3874cb78ccb92d574713fa
connahgriffin connahgriffin@gmail.com <connnahgriffin@gmail.com>
2020-08-20 19:46:10 +0530

    Codeblock layout

---
+++ codeblocks/UserTemplates/Chatbot/sources/Voice.h
@@ -0,0 +1,15 @@
+
+#ifndef _VOICE_H
+#define _VOICE_H
+
+#include <iostream>
+
+using namespace std;
+
+class Voice
+{
+    public:
+        void say(string phrase);    // Used to textually and audibly communicate a phrase
+};
+
+#endif
